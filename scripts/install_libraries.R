aaae0a1599e2d971d52c687966491475b007f0ab
hsm207 <hsm207@users.noreply.github.com>
2020-04-05 11:38:09 +0800

    Added library for pairwise plots

--- scripts/install_libraries.R
+++ scripts/install_libraries.R
@@ -3,3 +3,5 @@ install.packages("tidyverse", repos="https://cloud.r-project.org/", Ncpus=8)
 install.packages(c("devtools","mvtnorm","loo","coda"), repos="https://cloud.r-project.org/", dependencies=TRUE, Ncpus=8)
 library(devtools)
 install_github("rmcelreath/rethinking",ref="Experimental")
+
+install.packages("GGally")
