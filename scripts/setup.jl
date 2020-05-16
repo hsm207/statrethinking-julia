3693c6a995ebe4a4a42d5a49508f2475d146b855
hsm207 <hsm207@users.noreply.github.com>
2020-05-17 06:58:30 +0800

    Setup to create enviornment for Turing

--- scripts/setup.jl
+++ scripts/setup.jl
@@ -1,3 +1,7 @@
 using WebIO
+using IJulia
+
 jupyter = ENV["JUPYTER"]
 WebIO.install_jupyter_nbextension(`$jupyter`)
+
+installkernel("Julia (Turing)", "--project=/workspaces/statrethinking-gen/turing", specname = "julia-turing")
